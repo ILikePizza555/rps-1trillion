6bc03fe142e9b8d48ff0b8b101a71f82bd48d0fe
Izzy Lancaster <avrisaac555@gmail.com>
2023-10-22 15:16:15 -0700

    Use xo for linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 'use client';
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0/client.js';
 
 export default function Home() {
 	const { user, error, isLoading } = useUser();
 
-	if (isLoading) return <div>Loading...</div>;
-	if (error) return <div>{error.message}</div>;
-	if (user) return <div>
-		Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-	</div>;
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	if (error) {
+		return <div>{error.message}</div>;
+	}
+
+	if (user) {
+		return <div>
+		Welcome {user.name}! <a href='/api/auth/logout'>Logout</a>
+		</div>;
+	}
 
 	return <a href='/api/auth/login'>Login</a>;
 }
